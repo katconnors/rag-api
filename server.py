7e28a6dd56018620b5dda4e58cd4fa2a3d72778a
Katrina Connors <koktakatrina@gmail.com>
2025-01-02 22:43:13 -0800

    cleanup

--- server.py
+++ server.py
@@ -7,28 +7,22 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Welcome. Please make a post or get request."
+    return "Welcome. Please make a documented POST or GET request."
 
 
 @app.route("/ingest", methods=["POST"])
 def ingest_text():
 
-    # post method to ingest new text
-
     sentences = request.get_json()
-    print(sentences)
     embedded = rag.create_embeddings_post(sentences)
+
     return "Successfully ingested data."
 
 
 @app.route("/query")
 def get_similar_sentence():
 
-    # get method for user input and retrieving relevant sentence
-
     text = request.args.get("text")
-
-    print(rag.index)
 
     return rag.find_similar(text)
 
